b3803d3642ea05e85c4b00318efd5220f38aac8a
osakanatomozo <0yj3251k2p70v9f@i.softbank.jp>
2024-11-10 01:46:22 +0900

    課題用ユーザー情報作成

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // UserFactoryクラスで定義した内容にもとづいてダミーデータを100つ生成し、usersテーブルに追加する
-        User::factory()->count(100)->create();
+        // User::factory()->count(100)->create();
+
+        $user = new User();
+        $user->name = '侍 太郎';
+        $user->kana = 'サムライ タロウ';
+        $user->email = 'user@example.com';
+        $user->password = Hash::make('user_nagoyameshi');
+        $user->postal_code = '1050001';
+        $user->address = '東京都港区虎ノ門１丁目３−１';
+        $user->phone_number = '03-5790-9039';
+        $user->save();
     }
 }
