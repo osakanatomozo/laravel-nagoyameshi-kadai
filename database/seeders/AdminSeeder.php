f6aa8e688863c846d2cc64cbd555c560bd484402
osakanatomozo <0yj3251k2p70v9f@i.softbank.jp>
2024-11-10 01:30:17 +0900

    課題用データの作成

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,9 +14,14 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = new Admin();
-        $admin->email = 'admin@example.com';
-        $admin->password = Hash::make('nagoyameshi');
-        $admin->save();
+        $admin = Admin::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            ['password' => Hash::make('nagoyameshi')]
+        );
+
+        $admin2 = Admin::firstOrCreate(
+            ['email' => 'admin2@example.com'],
+            ['password' => Hash::make('admin2_nagoyameshi')]
+        );
     }
 }
